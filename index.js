addeeae2c1a499cbfaf75e0f262b0a71e991cafe
Brian Woodward <brian.woodward@gmail.com>
2014-12-11 18:06:01 -0500

    setup

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * assemble-init <https://github.com/assemble/assemble-init>
+ *
+ * Copyright (c) 2014 Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
